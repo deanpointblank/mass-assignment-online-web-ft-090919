0dab4344ccf6c727839711bbedf98eec28ce696e
Dean L Betty <deanlloyd64@gmail.com>
2019-09-27 00:32:25 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,7 +3,7 @@ class Person
   attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
   
   def initialize(new_person)
-    new_person.each do |key, value|
+    new_person.each do |'{key}=', value|
       self.send(key=, value)
     end
   end
